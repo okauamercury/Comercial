6a2740695bb65eb4820a8963df895187189a5897
-m <okauamercury>
2025-05-09 08:59:02 -0300

    Criando a classe Banco

--- ComercialTDSClass/Banco.cs
+++ ComercialTDSClass/Banco.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,22 +7,43 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace ComercialTDSClass
 {
     public static class Banco
     {
-        public static MySqlCommand Abrir()
+        
+        //List<object> lista = new();
+        public static string StrConn { get; set; }
+
+        public static MySql.Data.MySqlClient.MySqlCommand Abrir( string strconn ="")
         {
-            MySqlCommand cmd = new MySqlCommand();
-            string strconn = // colocar as informações da conexão
-
-
-
-                MySqlConnection cn = new(strconn);
-               try
-               {
+            MySqlCommand cmd = new();
+            StrConn = strconn;
+            if (StrConn == string.Empty) ;
+            StrConn = $"server=10.91.47.222;database=comercialtdsdb01;user=root;password=123";
+            MySqlConnection cn = new MySqlConnection(StrConn);
+            try
+            {
                 cn.Open();
-               }
+                cmd.Connection = cn;
+                               
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            return cmd;
+
+
         }
+
+
+
+
+
+
+
     }
 }

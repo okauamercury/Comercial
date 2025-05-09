c0c0b4a052ab921f3c43642c3b9e1be31c366888
-m <okauamercury>
2025-05-09 10:01:06 -0300

    Criando a classe nivel

--- ComercialTDSClass/Nivel.cs
+++ ComercialTDSClass/Nivel.cs
@@ -1,13 +1,55 @@
-﻿using System;
+﻿using Org.BouncyCastle.Asn1.Mozilla;
+using Org.BouncyCastle.Bcpg;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
 
 namespace ComercialTDSClass
 {
-    class Nivel
+    public class Nivel
     {
+        // propriedades
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Sigla { get; set; }
+
+        // construtor padrão
+        public Nivel() { }
+        
+        public Nivel( string nome, string sigla)
+        {
+            Nome = nome;
+            Sigla = sigla;
+        }
+        
+        public Nivel(int id, string nome, string sigla)
+        {
+            Id = id;
+            Nome = nome;
+            Sigla = sigla;
+        }
+        // inserir, atualizar, listar, ObterPorId(id)
+
+        public void Inserir()
+        {
+            var cmd = Banco.Abrir();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_nivel_insert";
+            //cmd.Parameters.Add("spnome", MySqlDbType.VarChar).Value = Nome;
+            cmd.Parameters.AddWithValue("spnome",Nome);
+            cmd.Parameters.AddWithValue("spsigla", Sigla);
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
+        }
+        public void Atualizar()
+        {
+            
+           
+        }
+
 
     }
 }

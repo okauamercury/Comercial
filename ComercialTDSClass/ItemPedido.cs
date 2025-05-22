3ae07aefbd16b84634afeed90c31a0909bd9e17a
-m <okauamercury>
2025-05-22 11:44:24 -0300

    Classes itempedido, pedido feitas.
    amanhã irei criar os forms.

--- ComercialTDSClass/ItemPedido.cs
+++ ComercialTDSClass/ItemPedido.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Mysqlx.Crud;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,7 +48,17 @@ namespace ComercialTDSClass
         // metodo da classe
         public void Deletar(int id)
         {
-
+            var item = ItemPedido.ObterPorId(id);
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"update estoques set " +
+                $"quantidade = quantidade + {item.Quantidade} " +
+                $"\n where produto_id = {item.Produto.Id};";
+            if (cmd.ExecuteNonQuery() > 0)
+            {
+                cmd.CommandText = $"delete from itempedido where id = {id}";
+                cmd.ExecuteNonQuery();
+            }
+            cmd.Connection.Close();
         }
 
         public void Inserir()
@@ -59,8 +71,70 @@ namespace ComercialTDSClass
             cmd.Parameters.AddWithValue("spproduto",Produto);
             cmd.Parameters.AddWithValue("spquantidade",Quantidade);
             cmd.Parameters.AddWithValue("spdesconto", Desconto);
+            Id = Convert.ToInt32(cmd.ExecuteScalar());
             
             cmd.Connection.Clone();
         }
+        public bool Atualizar()
+        {
+            
+            var cmd = Banco.Abrir();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "sp_itempedido_insert";
+            cmd.Parameters.AddWithValue("sppedidoid", PedidoID);
+            cmd.Parameters.AddWithValue("spproduto", Produto);
+            cmd.Parameters.AddWithValue("spquantidade", Quantidade);
+            bool atualizado = cmd.ExecuteNonQuery() > 0 ? true : false;
+            cmd.Parameters.AddWithValue("spdesconto", Desconto);
+           
+            return atualizado;
+        }
+        public static ItemPedido ObterPorId(int id)// id do item do pedido
+        {
+            ItemPedido itempedido = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select *from itempedidos where id = {id}";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                itempedido = new(
+                  dr.GetInt32(0),
+                  dr.GetInt32(1),
+                  Produto.ObterPorId(dr.GetInt32(2)),
+                  dr.GetDouble(3),
+                  dr.GetDouble(4),
+                  dr.GetDouble(5)
+                );
+               
+            }
+            dr.Close();
+            cmd.Connection.Close();
+            return itempedido;
+        }
+        public static List<ItemPedido> ObterListaPorPedidoId(int pedidoId)
+        {
+
+            List<ItemPedido> Items = new();
+            var cmd = Banco.Abrir();
+            cmd.CommandText = $"select *from itempedidos where pedido_id = {pedidoId}";
+            var dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Items.Add(new(
+                         dr.GetInt32(0),
+                         dr.GetInt32(1),
+                         Produto.ObterPorId(dr.GetInt32(2)),
+                         dr.GetDouble(3),
+                         dr.GetDouble(4),
+                         dr.GetDouble(5)
+                  )
+                );
+
+            }
+            dr.Close();
+            cmd.Connection.Close();
+            return Items;
+        }                                                  
+
     }
 }

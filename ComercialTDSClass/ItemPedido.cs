03a349d268e15ea7054d4c3223a412639f3ea9f9
-m <okauamercury>
2025-05-22 09:57:38 -0300

    Criando classe de pedido e itempedido

--- ComercialTDSClass/ItemPedido.cs
+++ ComercialTDSClass/ItemPedido.cs
@@ -6,8 +6,61 @@ using System.Threading.Tasks;
 
 namespace ComercialTDSClass
 {
-    class ItemPedido
+    public class ItemPedido
     {
 
+        public int Id { get; set; }
+        public int PedidoID { get; set; }
+        public Produto Produto { get; set; }
+        public double ValorUnit { get; set; }
+        public double Quantidade { get; set; }
+        public double Desconto { get; set; }
+       public ItemPedido()
+       {
+            Produto = new(); 
+       }
+        public ItemPedido(int id, int pedidoID, Produto produto, double valorUnit, double quantidade, double desconto)
+        {
+            Id = id;
+            PedidoID = pedidoID;
+            Produto = produto;
+            ValorUnit = valorUnit;
+            Quantidade = quantidade;
+            Desconto = desconto;
+        }
+        public ItemPedido(int pedidoID, Produto produto, double quantidade, double desconto)
+        {
+            PedidoID = pedidoID;
+            Produto = produto;
+            Quantidade = quantidade;
+            Desconto = desconto;
+        }
+        public ItemPedido(int id, double quantidade, double desconto)
+        {
+            Id = id;
+            Quantidade = quantidade;
+            Desconto = desconto;
+        }
+
+           
+        // metodo da classe
+        public void Deletar(int id)
+        {
+
+        }
+
+        public void Inserir()
+        {
+           
+            var cmd = Banco.Abrir();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "sp_itempedido_insert";
+            cmd.Parameters.AddWithValue("sppedidoid",PedidoID);
+            cmd.Parameters.AddWithValue("spproduto",Produto);
+            cmd.Parameters.AddWithValue("spquantidade",Quantidade);
+            cmd.Parameters.AddWithValue("spdesconto", Desconto);
+            
+            cmd.Connection.Clone();
+        }
     }
 }
---
+++ ComercialTDSDesk/FrmFornecedor.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace ComercialTDSDesk
+{
+    partial class FrmFornecedor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            txtNome = new TextBox();
+            txtCNPJ = new TextBox();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(88, 230);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(195, 230);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // txtNome
+            // 
+            txtNome.Location = new Point(88, 118);
+            txtNome.Name = "txtNome";
+            txtNome.PlaceholderText = "Nome";
+            txtNome.Size = new Size(138, 23);
+            txtNome.TabIndex = 2;
+            // 
+            // txtCNPJ
+            // 
+            txtCNPJ.Location = new Point(88, 156);
+            txtCNPJ.Name = "txtCNPJ";
+            txtCNPJ.PlaceholderText = "CNPJ";
+            txtCNPJ.Size = new Size(138, 23);
+            txtCNPJ.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(88, 185);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(138, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // FrmFornecedor
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox1);
+            Controls.Add(txtCNPJ);
+            Controls.Add(txtNome);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "FrmFornecedor";
+            Text = "FrmFornecedor";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox txtNome;
+        private TextBox txtCNPJ;
+        private TextBox textBox1;
+    }
+}
